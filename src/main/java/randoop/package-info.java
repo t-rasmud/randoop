1f012df25a864bd95174770addfd5a84ae7ab874
Jason Waataja <jwaataja@cs.washington.edu>
2019-12-23 16:05:57 -0800

    Use new @DefaultHasQualifierParameter

---
+++ src/main/java/randoop/package-info.java
@@ -0,0 +1,5 @@
+@DefaultHasQualifierParameter(NonDet.class)
+package randoop;
+
+import org.checkerframework.checker.determinism.qual.NonDet;
+import org.checkerframework.framework.qual.DefaultHasQualifierParameter;
